ee10cb2ac798fd5e9e4aa2ec648454a1fcc368ce
yuanwen <wen_yuan@cbit.com.cn>
2020-04-23 14:54:32 +0800

    日志管理

--- rtc/demos/js/log_management.js
+++ rtc/demos/js/log_management.js
@@ -15,13 +15,14 @@ $.ajax({
     //请求成功
     success : function(result) {         
         if(result.code == 200){
+          
             $("#userInfo").text(result.data.username);
             if(result.data.userRole == "3"){
                 $("#role_manager").hide();
             }
         }else if(result.code == 401){
             var url = "login.html"
-            window.location.href(url);
+            window.location.href = url;
         console.log(e.message);
     }
     },
@@ -49,120 +50,158 @@ var selections = []
     return res
   }
 
-  function detailFormatter(index, row) {
-    var html = []
-    $.each(row, function (key, value) {
-      html.push('<p><b>' + key + ':</b> ' + value + '</p>')
-    })
-    return html.join('')
-  }
+  // function detailFormatter(index, row) {
+  //   var html = []
+  //   $.each(row, function (key, value) {
+  //     html.push('<p><b>' + key + ':</b> ' + value + '</p>')
+  //   })
+  //   return html.join('')
+  // }
 
-  function operateFormatter(value, row, index) {
-    return [
-      '<a class="like" href="javascript:void(0)" title="Like">',
-      '<i class="fa fa-heart"></i>',
-      '</a>  ',
-      '<a class="remove" href="javascript:void(0)" title="Remove">',
-      '<i class="fa fa-trash"></i>',
-      '</a>'
-    ].join('')
-  }
+  // function operateFormatter(value, row, index) {
+  //   return [
+  //     '<a class="like" href="javascript:void(0)" title="Like">',
+  //     '<i class="fa fa-heart"></i>',
+  //     '</a>  ',
+  //     '<a class="remove" href="javascript:void(0)" title="Remove">',
+  //     '<i class="fa fa-trash"></i>',
+  //     '</a>'
+  //   ].join('')
+  // }
 
-  window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-      alert('You click like action, row: ' + JSON.stringify(row))
-    },
-    'click .remove': function (e, value, row, index) {
-      $table.bootstrapTable('remove', {
-        field: 'id',
-        values: [row.id]
-      })
-    }
-  }
+  // window.operateEvents = {
+  //   'click .like': function (e, value, row, index) {
+  //     alert('You click like action, row: ' + JSON.stringify(row))
+  //   },
+  //   'click .remove': function (e, value, row, index) {
+  //     $table.bootstrapTable('remove', {
+  //       field: 'id',
+  //       values: [row.id]
+  //     })
+  //   }
+  // }
 
-  function totalTextFormatter(data) {
-    return 'Total'
-  }
+  // function totalTextFormatter(data) {
+  //   return 'Total'
+  // }
 
-  function totalNameFormatter(data) {
-    return data.length
-  }
+  // function totalNameFormatter(data) {
+  //   return data.length
+  // }
 
-  function totalPriceFormatter(data) {
-    var field = this.field
-    return '$' + data.map(function (row) {
-      return +row[field].substring(1)
-    }).reduce(function (sum, i) {
-      return sum + i
-    }, 0)
-  }
+  // function totalPriceFormatter(data) {
+  //   var field = this.field
+  //   return '$' + data.map(function (row) {
+  //     return +row[field].substring(1)
+  //   }).reduce(function (sum, i) {
+  //     return sum + i
+  //   }, 0)
+  // }
 
   function initTable() {
     $table.bootstrapTable('destroy').bootstrapTable({
+      ajax:function(request){                    
+          $.ajax({
+              type : "POST",
+              dataType: "json",
+              //请求的媒体类型
+              contentType: "application/json;charset=UTF-8",
+              //请求地址
+              url : "http://117.78.9.153:24750/teamtalk/v1/log/getLogs",
+              //数据，json字符串
+              // data:JSON.stringify(postData),
+              xhrFields:{
+                  withCredentials:true
+              },             
+              success:function (res) {
+                if(res.code == 200){
+                  // console.log(res);
+                  
+                  request.success({
+                    row:res.data,
+                  });
+                  $('#table').bootstrapTable('load', res.data);
+                }else if(res.code == 401){
+                    var url = "login.html"
+                    window.location.href = url;
+                    console.log(e.message);
+                }                
+              },
+              error:function(error){
+                  console.log(error);
+              }
+          })
+      },
       height: 550,
       locale: $('#locale').val(),
       columns: [
+        // [{
+        //   field: 'state',
+        //   checkbox: false,
+        //   rowspan: 2,
+        //   align: 'center',
+        //   valign: 'middle'
+        // }, {
         [{
-          field: 'state',
-          checkbox: true,
-          rowspan: 2,
-          align: 'center',
-          valign: 'middle'
-        }, {
-          title: 'Item ID',
+          title: '用户ID',
           field: 'id',
           rowspan: 2,
           align: 'center',
           valign: 'middle',
           sortable: true,
-          footerFormatter: totalTextFormatter
+          width:150,
+          // footerFormatter: totalTextFormatter
         }, {
-          title: 'Item Detail',
-          colspan: 3,
-          align: 'center'
+          title: '变更记录',
+          colspan: 4,
+          align: 'center',
         }],
         [{
-          field: 'name',
-          title: 'Item Name',
-          sortable: true,
-          footerFormatter: totalNameFormatter,
+          field: 'eventUserId',
+          title: '姓名',
+          sortable: false,
+          // footerFormatter: totalNameFormatter,
           align: 'center'
         }, {
-          field: 'price',
-          title: 'Item Price',
-          sortable: true,
+          field: 'eventUserRole',
+          title: '角色',
+          sortable: false,
           align: 'center',
-          footerFormatter: totalPriceFormatter
-        }, {
-          field: 'operate',
-          title: 'Item Operate',
+        },{
+          field: 'eventName',
+          title: '操作',
+          sortable: false,
           align: 'center',
-          clickToSelect: false,
-          events: window.operateEvents,
-          formatter: operateFormatter
+          width:300,
+        },{
+          field: 'createTime',
+          title: '时间',
+          sortable: false,
+          align: 'center',
+          width:300,
         }]
       ]
     })
-    $table.on('check.bs.table uncheck.bs.table ' +
-      'check-all.bs.table uncheck-all.bs.table',
-    function () {
-      $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
+    // $table.on('check.bs.table uncheck.bs.table ' +
+    //   'check-all.bs.table uncheck-all.bs.table',
+    // function () {
+    //   $remove.prop('disabled', !$table.bootstrapTable('getSelections').length)
 
-      // save your data, here just save the current page
-      selections = getIdSelections()
-      // push or splice the selections if you want to save all data selections
-    })
-    $table.on('all.bs.table', function (e, name, args) {
-      console.log(name, args)
-    })
-    $remove.click(function () {
-      var ids = getIdSelections()
-      $table.bootstrapTable('remove', {
-        field: 'id',
-        values: ids
-      })
-      $remove.prop('disabled', true)
-    })
+    //   // save your data, here just save the current page
+    //   selections = getIdSelections()
+    //   // push or splice the selections if you want to save all data selections
+    // })
+    // $table.on('all.bs.table', function (e, name, args) {
+    //   console.log(name, args)
+    // })
+  //   $remove.click(function () {
+  //     var ids = getIdSelections()
+  //     $table.bootstrapTable('remove', {
+  //       field: 'id',
+  //       values: ids
+  //     })
+  //     $remove.prop('disabled', true)
+  //   })
   }
 
   $(function() {
@@ -235,59 +274,3 @@ function role_management(){
 }
 
 
-
-function logList(){ 
-    $.ajax({
-        //请求方式
-        type : "POST",
-        //请求的媒体类型
-        contentType: "application/json;charset=UTF-8",
-        //请求地址
-        url : "http://117.78.9.153:24750/teamtalk/v1/log/getlogs",
-        //数据，json字符串
-        xhrFields:{
-            withCredentials:true
-        },
-        dataType: "json",
-        //请求成功
-        success : function(result) {
-            if(result.code == 200){
-                var userList = result.data;
-                var userUl = document.getElementById('userListGroup');
-                $('#userListGroup li').remove();
-                var liHead = document.createElement("li");
-                liHead.innerHTML = '<li class="list-group-item active">' + '管辖人员' +'</li>';
-                userUl.appendChild(liHead);
-                
-                for(i = 0,len = userList.length; i < len; i++){
-                    let li = document.createElement("li");
-                    let userId = userList[i].userId;
-                    let userName = userList[i].userName;
-                    // let userSec = userList[i].userSec;
-                    let userRole = userList[i].userRole;
-                    li.onclick= function(){
-                        $("#userId").html(userId);
-                        $("#role_type").val(userRole);
-                    }
-                    
-                    li.innerHTML = '<li class="list-group-item">\n'+
-                    '<div id="" class="meeting-item" onclick="" data-toggle="modal" data-target="#userModal">'+userId+'.'+userName+'</div>'+
-                    
-                    '<span class="meetingInfo">'+ userRole + '</span>'+
-                    '</li>'
-                    userUl.appendChild(li);
-                }
-            }else if(result.code == 401){
-                var url="login.html";
-                window.location.href = url
-                alert(result.message);
-                    
-            }
-        },
-    error:function(e){
-        
-        console.log(e.message); 
-    }
-    })
-};
-// window.onload = roleList();
